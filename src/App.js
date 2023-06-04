beae61d3c07865744d86139f2ca95bf424dddca4
Fouzan Talha Adil <fouzankhan2000@gmail.com>
2023-06-04 15:23:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { MdCloudUpload, MdDelete } from 'react-icons/md';
+import { AiFillFileImage } from 'react-icons/ai';
 
 function App() {
+
+  const [image, setImage] = useState(null);
+  const [fileName, setFileName] = useState("No selected file");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <form 
+      onClick={() => document.querySelector(".input-field").click()}
+      >
+        <input type='file' accept='.csv' className='input-field' hidden 
+          onChange={({ target: {files}}) => {
+            files[0] && setFileName(files[0].name)
+          }}
+        />
+
+        {image ?
+        <img src={image} width={150} height={150} alt={fileName} />
+        :
+        <>
+        <MdCloudUpload color='#1475cf' size={60} />
+        <p>Select a CSV file to upload</p>
+        </>}
+      </form>
+
+      <section className='uploaded-row'>
+        <AiFillFileImage color='#1475cf' />
+        <span className='upload-content'>
+          {fileName} -
+          <MdDelete 
+            onClick={() => {
+              setFileName("No selected File")
+              setImage(null)
+            }}
+          />
+        </span>
+      </section>
+    </main>
   );
 }
 
